dbf93dee0c06dd5012bd47126d40efb508425ae9
VictorFerrer <victor.ferrer@aspentech.com>
2021-07-07 17:58:50 -0500

    Addition of new Tests

---
+++ Simulators/Tests/OpenCloseCaseRepeatedly.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulators.Tests
+{
+    public class OpenCloseCaseRepeatedly
+    {
+        public static void TestDefinition(string filePath, string fileName, ISimulator hysysSimulator)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (hysysSimulator.OpenCase(new CaseInfo(filePath, fileName)))
+                    hysysSimulator.CloseCase();
+            }
+        }
+    }
+}
---
+++ Simulators/Tests/SaveCaseRepeatedly.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulators.Tests
+{
+    public class SaveCaseRepeatedly
+    {
+        public void TestDefinition(string fileName, string filePath, ISimulator hysysSimulator)
+        {
+            bool isOpen = hysysSimulator.OpenCase(new CaseInfo(filePath, fileName));
+            if (isOpen)
+            {
+                for (int i = 0; i < 10; i++)
+                    hysysSimulator.SaveCase();
+            }
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
